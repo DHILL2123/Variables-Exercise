d1f1ef0afea2a98b8092d9eb014c80c8ec0b95ac
DeAdrien Hill <deadrienhill@yahoo.com>
2022-11-21 14:37:09 -0600

    Declared and initialized variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿// Delcare and Initialize variables
+string name = "Six";
+int age = 4;
+char middleInitial = 'D';
+bool isMale = true;
+double currentTemp = 88.9;
+decimal currentPrice = 50.00m;
+
+Console.WriteLine($"My dogs Name is: {name}");
+Console.WriteLine($"His Age is: {age}");
+Console.WriteLine($"His Middle Initial is: {middleInitial}");
+Console.WriteLine($"He is male: {isMale}");
+Console.WriteLine($"His current temp is: {currentTemp}");
+Console.WriteLine($"The sale price is: {currentPrice}");
